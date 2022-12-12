568e8f76cab62f1630cc77d57bf86750430ffc26
bradleecrockett <19703290+bradleecrockett@users.noreply.github.com>
2022-12-12 09:48:23 -0800

    Create Prob01.java

---
+++ 2017/Prob01.java
@@ -0,0 +1,7 @@
+// Note the naming convention for solution: probXX.java where XX is the 2 digit problem number (no capitalization) 
+public class prob01.java{
+  public static void main(String[] args){
+    //Your code here
+    
+  }
+}
