fb8d96c309a7b9d6a0b10cff22546cef9b3f2cd8
bradleecrockett <bcrockett@rjuhsd.us>
2023-02-16 10:36:33 -0800

    Completed 2019 prob11

---
+++ 2019/prob11.py
@@ -0,0 +1,49 @@
+
+# Output sample
+'''
+7  +-----+ 
+6  |     | 
+5   /\       |     | 
+4  +-+       |     | 
+3  | | +-+   | ____|__    +---+ 
+2 \| | | |   |/       \   |   | 
+1  \ | | |   /         \  |   | 
+0   +-------+           +-+---+ 
+0123456789012345678901234567890
+'''
+
+#Sample Input
+'''
+30 8  
+2 \| | | |   |/       \   |   | 
+0   +-------+           +-+---+ 
+1  \ | | |   /         \  |   | 
+5   /\       |     | 
+7  +-----+ 
+3  | | +-+   | ____|__    +---+ 
+4  +-+       |     | 
+6  |     |
+'''
+path = '2019/2019data/prob11-1-in.txt'
+handle = open(path) # open the file handle
+full_text = handle.read()
+# print(full_text)
+lines = full_text.split('\n')
+
+line_1 = lines[0]
+width = line_1.split()[0]
+num_lines = line_1.split()[1]
+
+print(width)
+print(num_lines)
+pic_list = lines[1:]
+pic_list.sort()
+for l in reversed(pic_list):
+    print(l)
+nums = '0123456789'
+bottom = ''
+index = 0
+for i in range(int(width)+1):
+    bottom += nums[index%10]
+    index += 1
+print(bottom)
