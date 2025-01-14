e448fd67bf68e568a42d1a82f9dcbeeec72f65a3
Brad C <bcrockett@rjuhsd.us>
2025-01-14 15:04:44 -0800

    completed prob 08 2023

--- 2023/prob08.py
+++ 2023/prob08.py
@@ -1,8 +1,16 @@
-a = int(input())
-n = int(input())
-p = 0
-while True:
-    if a ** p == n:
-        print(f"{a}^{p} = {n}")
-        break
-    p += 1
+
+
+# given 2 integers, the 1st is the base
+# the 2nd is the exp
+
+# figure out what the power was....
+
+base = int(input())
+power = int(input())
+
+new_pow = 1* base
+count = 1
+while power > new_pow:
+    new_pow*= base
+    count += 1
+print(base, "^" ,count,"=",power)
